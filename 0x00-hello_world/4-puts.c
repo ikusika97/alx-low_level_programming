4ed6c37dbed063263e176da11ea10b4309efafa6
ikusika joseph <ikusikajoseph@yahoo.com>
2023-02-16 14:31:31 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - print a of code line using puts
+ *
+ *Return: alwways 0 
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  retrun (0);
+}
